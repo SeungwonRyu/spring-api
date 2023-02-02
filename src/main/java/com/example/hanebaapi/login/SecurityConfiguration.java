b1ffe02f5e5c872e8e804e59865afae5a876b108
SeungwonRyu <mikeryu98@gmail.com>
2023-02-02 22:39:39 +0900

    23.02.02  - 로그인 SecurityFilterChain 구현

--- src/main/java/com/example/hanebaapi/login/SecurityConfiguration.java
+++ src/main/java/com/example/hanebaapi/login/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
 @Configuration
@@ -16,6 +17,41 @@ public class SecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder pwEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        // Http Permission
+        http.authorizeHttpRequests()
+                .requestMatchers("/", "/user/**", "/error/**", "/js/**", "/css/**", "/image/**")
+                .permitAll().anyRequest().authenticated();
+
+        // Login
+        http.formLogin()
+                .loginPage("/id/login").loginProcessingUrl("/login/action")
+                .successHandler(authSuccessHandler).failureHandler(authFailureHandler);
+
+        // Logout
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/id/logout"))
+                .logoutSuccessUrl("/id/login").invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me");
+
+        // Session
+        http.sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false)
+                .expiredUrl("/login?error=true&exception=Have been attempted to login from a new place. or session expired");
+
+        // 유지
+        http.rememberMe()
+                .key("0467EC591838570F48CC386CEE6ED9FBA53B4593A283BAFD5A94347AD3428408")
+                .alwaysRemember(false).tokenValiditySeconds(43200)
+                .rememberMeParameter("remember-me");
+
+        return http.build();
     }
 
 }
