691dbe0e69fb286c6c202f8733434061ff201967
Ryu <80870887+SeungwonRyu@users.noreply.github.com>
2023-02-09 17:43:17 +0900

    23.02.09  - Jwt Testing

--- src/main/java/com/example/hanebaapi/loginApp/config/JwtProvider.java
+++ src/main/java/com/example/hanebaapi/loginApp/config/JwtProvider.java
@@ -1,17 +1,25 @@
 package com.example.hanebaapi.loginApp.config;
 
 import com.example.hanebaapi.loginApp.service.JwtUserDetailsService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Base64;
+import java.util.Date;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
 public class JwtProvider {
-    @Value("spring.jwt.secret")
+    //@Value("${jwt.secret}")
     private String secretKey;
     private Long tokenValidMillisecond = 60 * 60 * 1000L;
     private final JwtUserDetailsService jwtUserDetailsService;
@@ -21,6 +29,30 @@ public class JwtProvider {
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
     }
 
+    // Jwt 생성
+    public String createToken(String userPk, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(userPk);
+        claims.put("roles", roles);
 
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenValidMillisecond))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    // 인증정보 조회
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(this.getUserPk(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // 회원 구분 pk 추출
+    public String getUserPk(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
 }
 
